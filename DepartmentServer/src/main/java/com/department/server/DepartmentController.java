502fa38cfd3dadf5534533d7e4e6b6c68d40932b
rgavhale1 <rgavhale1994@gmail.com>
2022-05-02 17:39:44 +0530

    test

--- DepartmentServer/src/main/java/com/department/server/DepartmentController.java
+++ DepartmentServer/src/main/java/com/department/server/DepartmentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/department")
+@RequestMapping("/department") 
 public class DepartmentController {
 
 	@Autowired
