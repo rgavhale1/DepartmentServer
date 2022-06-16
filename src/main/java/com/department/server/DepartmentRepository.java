419b951bfde087fd60467203491371eb99f48d96
rgavhale1 <rgavhale1994@gmail.com>
2022-06-16 14:10:40 +0530

    testing intellij

--- src/main/java/com/department/server/DepartmentRepository.java
+++ src/main/java/com/department/server/DepartmentRepository.java
@@ -2,7 +2,7 @@ package com.department.server;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
+//Department Repository
 @Repository
 public interface DepartmentRepository extends  MongoRepository<Department,Long>{
 
