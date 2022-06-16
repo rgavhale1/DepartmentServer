f9ecfd648de25d545efddad84ffbe1a5e627aa52
rgavhale1 <rgavhale1994@gmail.com>
2022-06-16 16:01:13 +0530

    testing intellij

--- src/main/java/com/department/server/DepartmentServerApplication.java
+++ src/main/java/com/department/server/DepartmentServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 //Department Application
+////////
 @SpringBootApplication
 @EnableEurekaClient
 public class DepartmentServerApplication {
